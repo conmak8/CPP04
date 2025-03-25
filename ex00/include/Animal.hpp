7dccfc54d3485dc6b81fd25ab2a2c50acfbb7b64
Constantinos Makariou <cmakario@student.42heilbronn.de>
2025-03-25 08:26:11 +0100

    00: Start of headers

---
+++ ex00/include/Animal.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Animal.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cmakario <cmakario@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/25 08:05:24 by cmakario          #+#    #+#             */
+/*   Updated: 2025/03/25 08:17:46 by cmakario         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ANIMAL_HPP
+# define ANIMAL_HPP
+
+# include <iostream>
+
+class Animal
+{
+protected:
+		std::string type;
+public:
+		void sound();
+}
+
+#endif // !ANIMAL_HPP
---
+++ ex00/include/Dog.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Dog.hpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cmakario <cmakario@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/25 08:21:31 by cmakario          #+#    #+#             */
+/*   Updated: 2025/03/25 08:24:52 by cmakario         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef DOG_HPP
+# define DOG_HPP
+
+# include "Animal.hpp"
+
+class Dog : public Animal
+{
+protected:
+		std::string type;
+public:
+		void sound();
+};
+
+#endif // !DOG_HPP
