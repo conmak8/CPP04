8422cfd2d733101a8616157c4a78d9dc6756c6da
Constantinos Makariou <cmakario@student.42heilbronn.de>
2025-03-27 22:15:07 +0100

    updated version of main

--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: cmakario <cmakario@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/25 10:19:54 by cmakario          #+#    #+#             */
-/*   Updated: 2025/03/26 21:18:09 by cmakario         ###   ########.fr       */
+/*   Updated: 2025/03/27 22:12:25 by cmakario         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,16 +20,40 @@ int main() {
 	// A_Animal* animal = new A_Animal();	// 🚫 NOT ALLOWED
 	// A_Animal animal;						// 🚫 NOT ALLOWED
 	
-	A_Animal* animals[2];
+	// A_Animal* animals[2];
 
-	animals[0] = new Dog();					// ✅ 
-	animals[1] = new Cat();					// ✅ 
+	// animals[0] = new Dog();					// ✅ 
+	// animals[1] = new Cat();					// ✅ 
 
-	for (int i = 0; i < 2; ++i)
-		animals[i]->makeSound();
+	// for (int i = 0; i < 2; ++i)
+	// 	animals[i]->makeSound();
 
-	for (int i = 0; i < 2; ++i)
-		delete animals[i];
+	// for (int i = 0; i < 2; ++i)
+	// 	delete animals[i];
 
+	// return 0;
+
+
+	// Initialization of all three types:
+	const Dog* dog = new Dog();
+	const Cat* cat = new Cat();
+	
+	// testing the inherited getType member function:
+	std::cout << std::endl;
+	std::cout << "Object a is of type: " << dog->getType() << std::endl;
+	std::cout << "Object b is of type: " << cat->getType() << std::endl;
+	std::cout << std::endl;
+	
+	// testing the overriden makeSound method:
+	dog->makeSound();
+	cat->makeSound();
+	std::cout << std::endl;
+
+	delete cat;
+	delete dog;
+	
 	return 0;
+
+
+	
 }
