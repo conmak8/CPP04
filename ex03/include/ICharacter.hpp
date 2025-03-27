320150fb92689eaf449fafba644635c0c28e86a4
cmakario <cmakario@student.42.fr>
2025-03-27 07:30:19 +0100

    04: ICharacter header created

---
+++ ex03/include/ICharacter.hpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ICharacter.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cmakario <cmakario@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/27 07:06:40 by cmakario          #+#    #+#             */
+/*   Updated: 2025/03/27 07:24:33 by cmakario         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ICHARACTER_HPP
+# define ICHARACTER_HPP
+
+# include <iostream>
+# include "AMateria.hpp" // * Forward Declaration abstract class AMateria
+
+class AMatertia; // * Forward Declaration
+
+class ICharacter
+{
+public:
+		virtual ~ICharacter() {};
+
+		virtual std::string const &getName() const = 0;
+		virtual void equip(AMateria *m) = 0;
+		virtual void unequip(int idx) = 0;
+		virtual void use(int idx, ICharacter &target) = 0;
+
+};
+
+#endif // !ICHARACTER_HPP
